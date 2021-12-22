323d2bef93fde9f9a7f71e748e1b229c4e500ca5
DrPeterPun <pedro_pereira@outlook.pt>
2021-12-22 18:01:27 +0000

    i hope everything is alraight

--- src/ClientInfo.java
+++ src/ClientInfo.java
@@ -3,13 +3,13 @@ import java.net.InetAddress;
 public class ClientInfo {
     InetAddress adress;
     int router; // closest router
-    public ClientInfo(InetAddress address, int router){
-       this.adress = adress;
+    public ClientInfo(InetAddress adr, int router){
+       this.adress = adr;
        this.router = router;
     }
     
     @Override
     public boolean equals(Object obj) {
-        return this.adress.equals((ClientInfo) obj.adress );
+        return this.adress.equals(((ClientInfo) obj).adress );
     }
 }
